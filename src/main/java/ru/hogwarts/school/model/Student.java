4cccd9acfe660527b88aee72e1f46d2fb9cf39d3
Acer <chukurnev@mail.ru>
2023-08-01 04:48:40 +0400

    Тесты

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -15,6 +15,12 @@ public class Student {
     private String name;
     private int age;
 
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
     @ManyToOne
     private  Faculty faculty;
 
---
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerIntegrationTest.java
@@ -0,0 +1,65 @@
+package ru.hogwarts.school.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FacultyControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private FacultyRepository facultyRepository;
+
+    @AfterEach
+    public void resetDb() {
+        facultyRepository.deleteAll();
+    }
+
+    @Test
+    void shouldCreateFaculty(){
+        // given
+        Faculty faculty = new Faculty();
+        faculty.setName("Gryffindor");
+        faculty.setColor("Color");
+
+        // when
+        ResponseEntity<Faculty> response = restTemplate.postForEntity("/faculties",faculty,Faculty.class);
+
+        // then
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody().getId()).isNotNull();
+        Assertions.assertThat(response.getBody().getName()).isEqualTo(faculty.getName());
+        Assertions.assertThat(response.getBody().getColor()).isEqualTo(faculty.getColor());
+    }
+    @Test
+    void shouldGetFaculty() {
+        // given
+        Long facultyId = persistTestFaculty("Gryffindor", "Color").grtId;
+
+        // when
+        ResponseEntity<Faculty> responseEntity = restTemplate.getForEntity("/faculties/{id}", Faculty.class, facultyId);
+
+        // then
+        Faculty faculty = responseEntity.getBody();
+        Assertions.assertThat(faculty).isNotNull;
+        Assertions.assertThat(faculty.getId()).isEqualTo(facultyId);
+
+        private Faculty persistTestFaculty(String name, String color) {
+            Faculty faculty = new Faculty(name, color);
+            return facultyRepository.save(faculty);
+        }
+    }
+}
+
+
